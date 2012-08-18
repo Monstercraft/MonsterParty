c911523fe87a6e7dea9564028ec156afd7ed8cf3
Fletch to 99 <fletchto99@hotmail.com>
2012-08-18 19:11:01 -0400

    fix for building

--- src/main/java/org/monstercraft/party/plugin/command/commands/Message.java
+++ src/main/java/org/monstercraft/party/plugin/command/commands/Message.java
@@ -1,7 +1,26 @@
 package org.monstercraft.party.plugin.command.commands;
 
+import org.bukkit.command.CommandSender;
 import org.monstercraft.party.plugin.command.GameCommand;
 
 public class Message extends GameCommand{
+
+	@Override
+	public boolean canExecute(CommandSender sender, String[] split) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean execute(CommandSender sender, String[] split) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String[] getPermission() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
