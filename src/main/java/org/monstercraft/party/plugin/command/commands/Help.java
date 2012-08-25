69efcd0b3a0e2bbdea3d40eccb29ef662132b4ba
Jurre Koetse <Jurre_Dev@koetse.eu>
2012-08-25 19:28:14 +0200

    Changed permission based
    Broken

--- src/main/java/org/monstercraft/party/plugin/command/commands/Help.java
+++ src/main/java/org/monstercraft/party/plugin/command/commands/Help.java
@@ -23,24 +23,11 @@ public class Help extends GameCommand {
 	@Override
 	public boolean execute(CommandSender sender, String[] split) {
 		Player player = (Player) sender;
-		player.sendMessage(ChatColor.RED + "                          MonsterParty Help");
-		player.sendMessage(ChatColor.DARK_PURPLE + "-----------------------------------------------------");
-		player.sendMessage(ChatColor.YELLOW + "/party create [Name] " + ChatColor.AQUA + "- Create a party");
-		player.sendMessage(ChatColor.YELLOW + "/party create [Name] :p[Password] " + ChatColor.AQUA + "- Create Password Protected Party");
-		player.sendMessage(ChatColor.YELLOW + "/party lock " + ChatColor.AQUA + "- Lock your party");
-		player.sendMessage(ChatColor.YELLOW + "/party join [Name] " + ChatColor.AQUA + "- Join a party");
-		player.sendMessage(ChatColor.YELLOW + "/party leave " + ChatColor.AQUA + "- Leave your party");
-		player.sendMessage(ChatColor.YELLOW + "/party list " + ChatColor.AQUA + "- List available parties or members in your party");
-		player.sendMessage(ChatColor.YELLOW + "/party invite [Player] " + ChatColor.AQUA + "- Invite a player to the party [Owner]");
-		player.sendMessage(ChatColor.YELLOW + "/party kick [Player] " + ChatColor.AQUA + "- Kick a player from the part [Owner]");
-		player.sendMessage(ChatColor.YELLOW + "/party teleport [player] " + ChatColor.AQUA + "- Teleport to a party member");
-		player.sendMessage(ChatColor.YELLOW + "/ptp [Player] " + ChatColor.AQUA + "- Teleport to a party member");
-		player.sendMessage(ChatColor.YELLOW + "/pc [msg] " + ChatColor.AQUA + "- Send a message to party chat");
-		player.sendMessage(ChatColor.YELLOW + "/pc " + ChatColor.AQUA + "- Toggle PartyChat");
-		player.sendMessage(ChatColor.DARK_PURPLE + "-----------------------------------------------------");
-		player.sendMessage(ChatColor.WHITE + "Plugin by: " + ChatColor.RED + "Fletch_to_99 " + ChatColor.WHITE + "Help Created by: " + ChatColor.RED + "Jurre1996");
-		player.sendMessage(ChatColor.DARK_PURPLE + "-----------------------------------------------------");
-		return true;
+		if (player.hasPermission("monsterparty.create")) {
+			sender.sendMessage(ChatColor.YELLOW + "/party create [Name] " + ChatColor.AQUA + "- Create a party");
+			return true;
+		}
+		return false;
 	}
 
 	@Override
